351d148cb40f729cb11eeec5138b615c211c7668
LeonardoHSSoares <mascoty2005@gmail.com>
2023-11-29 20:16:21 -0300

    fix: 🐞

---
+++ FirstConditional/if_else.java
@@ -0,0 +1,12 @@
+package FirstConditional;
+
+public class if_else {
+
+    public static void main(String[] args) {
+        
+        String nome = args[0]
+        if (condition) {
+            
+        }
+    }
+}
