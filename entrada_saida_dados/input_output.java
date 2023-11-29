3399c9bb5f63e654015f9f6d5b61017b602c8c4a
LeonardoHSSoares <mascoty2005@gmail.com>
2023-11-29 20:38:41 -0300

    🎉 init(Input_Output):

---
+++ entrada_saida_dados/input_output.java
@@ -0,0 +1,12 @@
+package entrada_saida_dados;
+
+public class input_output {
+
+    public static void main(String[] args) {
+        
+        String nome = args[0];
+        
+        System.out.print("\nOlá "+nome+" , seja Bem Vindo!\n\n");
+        }
+    }
+
