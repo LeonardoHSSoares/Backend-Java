1c64183c400ca53762aba97704a766bb15faf221
LeonardoHSSoares <mascoty2005@gmail.com>
2023-11-29 19:28:40 -0300

    feat(add-class): ✨

--- HelloWorld/helloWorld.java
+++ HelloWorld/helloWorld.java
@@ -1,9 +1,12 @@
 package HelloWorld;
 
 public class helloWorld {
-    public static void main(String[] args) {
 
-        System.out.println("Hello World");
+    public static void main(String[] args){
         
+        System.out.printf("\nOlá Mundoooo Java!!\n");
+        System.out.printf("\nJa me livrei da Maldição!!\n\n");
+        System.out.printf("\n");
     }
-}
+    
+};
