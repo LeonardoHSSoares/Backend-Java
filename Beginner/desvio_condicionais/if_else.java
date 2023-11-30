01d16dbc22db716a0a1ba81f90ab0bb26cf1dd32
LeonardoHSSoares <mascoty2005@gmail.com>
2023-11-30 11:40:03 -0300

    🎉 init: condicionais

---
+++ Beginner/desvio_condicionais/if_else.java
@@ -0,0 +1,21 @@
+package desvio_condicionais;
+
+public class if_else {
+
+    public static void main(String[] args){
+
+        String nome = args[0];
+
+        if (nome.equalsIgnoreCase("dev")) {
+            System.out.print("\nExecutando em modo Desenvolvedor\n");
+        }else if(nome.equalsIgnoreCase("user")){
+            System.out.print("\nExecutando modo Usuario\n");
+        }else {
+
+            System.out.print("\nBem Vindo "+nome+"\n");
+        }
+    }
+}
+
+
+
