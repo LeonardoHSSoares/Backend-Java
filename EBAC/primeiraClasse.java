42efdd8fedf7913610760a0e58a9ddc3960ffbf6
LeonardoHSSoares <mascoty2005@gmail.com>
2023-12-01 19:12:53 -0300

    🎉 init: Primeiro Projeto

---
+++ EBAC/primeiraClasse.java
@@ -0,0 +1,10 @@
+package EBAC;
+
+public class primeiraClasse {
+
+    public static void main(String[] args) {
+        String nome = "Leonardo";
+
+        System.out.print("\nHello "+nome+" Seja Bem Vindo!\n\n" );
+    }
+}
