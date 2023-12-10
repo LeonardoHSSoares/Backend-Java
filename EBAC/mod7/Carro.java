5416500c1c9f49561df01c15b86ccd8981c2baa3
LeonardoHSSoares <mascoty2005@gmail.com>
2023-12-10 10:04:47 -0300

    criado classe Carro com alguns getters

--- EBAC/mod7/Carro.java
+++ EBAC/mod7/Carro.java
@@ -13,32 +13,25 @@ public class Carro {
         this.cor = cor;
     }
 
-    // Método para obter o modelo do carro
     public String getModelo() {
+
         return modelo;
     }
 
-    // Método para obter o ano do carro
     public int getAno() {
+
         return ano;
     }
 
-    // Método para obter a cor do carro
     public String getCor() {
+
         return cor;
     }
 
-    // Método para exibir informações sobre o carro
     public void exibirInformacoes() {
-        System.out.println("Carro: " + modelo + " | Ano: " + ano + " | Cor: " + cor);
+        System.out.println("Carro: " + modelo);
+        System.out.println("Ano: " + ano);
+        System.out.println("Cor: " + cor);
     }
 
-    // Método principal (para testar a classe Carro)
-    public static void main(String[] args) {
-        // Criando uma instância da classe Carro
-        Carro meuCarro = new Carro("Fusca", 1970, "Azul");
-
-        // Chamando o método para exibir informações sobre o carro
-        meuCarro.exibirInformacoes();
-    }
 }
