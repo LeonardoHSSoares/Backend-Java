399ebde4d3ca8a55982bc0646e3ade11c786214e
LeonardoHSSoares <mascoty2005@gmail.com>
2023-12-11 20:22:20 -0300

    🐞 fix:

---
+++ Beginner/praticandoClasses/Console.java
@@ -0,0 +1,16 @@
+package praticandoClasses;
+
+import EBAC.mod5.primeiroProjeto.primeiraClasse;
+
+public class Console {
+
+    private String fabricante;
+    private String marca;
+    
+    public Console(String fabricante,String marca) {
+        this.fabricante = fabricante;
+        this.marca = marca;
+    }
+    
+    
+}
---
+++ Beginner/praticandoClasses/Main.java
@@ -0,0 +1,7 @@
+package praticandoClasses;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
