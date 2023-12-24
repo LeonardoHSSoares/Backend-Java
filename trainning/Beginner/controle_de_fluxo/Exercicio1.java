b052929cac711f395fd5c6fc7ac1f8b5fe76e6cc
LeonardoHSSoares <mascoty2005@gmail.com>
2023-12-24 17:49:31 -0300

    🐞 fix:

--- trainning/Beginner/controle_de_fluxo/Exercicio1.java
+++ trainning/Beginner/controle_de_fluxo/Exercicio1.java
@@ -1,5 +1,34 @@
 package controle_de_fluxo;
 
+import java.util.Scanner;
+
 public class Exercicio1 {
-    
+
+    public static void main(String[] args) {
+
+        coletaNum();
+
+    }
+
+    public static void coletaNum() {
+
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite um numero inteiro: ");
+        int iNum = leia.nextInt();
+
+        leia.close();
+        desenhaLinha();
+        System.out.println("Numero antes da conversão: " + iNum);
+        desenhaLinha();
+
+    }
+
+    public static void desenhaLinha() {
+
+        for (int i = 0; i < 65; i++) {
+            System.out.print("-");
+
+        }
+    }
 }
