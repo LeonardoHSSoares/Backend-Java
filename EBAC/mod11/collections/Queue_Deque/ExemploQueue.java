e33ecfff89dad9c0bd8923d2474e45a1c6dce451
LeonardoHSSoares <mascoty2005@gmail.com>
2023-12-30 13:25:36 -0300

    🎉 init(Queue):

---
+++ EBAC/mod11/collections/Queue_Deque/ExemploQueue.java
@@ -0,0 +1,13 @@
+package EBAC.mod11.collections.Queue_Deque;
+
+import java.util.Deque;
+
+public class ExemploQueue {
+
+    
+    public static void main(String[] args) {
+       
+        };
+        
+    
+}
