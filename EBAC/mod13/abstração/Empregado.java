ea78f88ebb59d9bc9a057a678332134a3805527b
LeonardoHSSoares <mascoty2005@gmail.com>
2024-01-06 14:42:34 -0300

    🧪 test:

--- EBAC/mod13/abstração/Empregado.java
+++ EBAC/mod13/abstração/Empregado.java
@@ -1,5 +1,26 @@
 package EBAC.mod13.abstração;
 
+/**
+ * Classe abstrata Empregado
+ */
 public abstract class Empregado {
     
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+
+    public String getNome() {
+        return nome;
+    }
+    public String getSobrenome(){
+        return sobrenome;
+    }
+    public String getCpf(){
+        return cpf;
+    }
+
+    public Double vencimento() {
+        
+    }
+
 }
