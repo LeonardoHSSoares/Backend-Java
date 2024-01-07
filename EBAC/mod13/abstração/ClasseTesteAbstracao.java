b688a613f37749729392b0e63ca0c62cfdc4a9a4
LeonardoHSSoares <mascoty2005@gmail.com>
2024-01-07 09:20:41 -0300

    ✨ feat:

--- EBAC/mod13/abstração/ClasseTesteAbstracao.java
+++ EBAC/mod13/abstração/ClasseTesteAbstracao.java
@@ -12,21 +12,31 @@ public class ClasseTesteAbstracao {
         empregadoHorista.setNome("Leonardo");
         empregadoHorista.setSobrenome("Soares");
         empregadoHorista.setCpf("1");
-
+        System.out.println(empregadoHorista.getNome());
+        System.out.println(empregadoHorista.getSobrenome());
+        System.out.println(empregadoHorista.getCpf());
 
         System.out.println("\n");
         
         //Assalariado
         System.out.println(empregadoAssalariado.getClass().getSimpleName());
-
-
+        empregadoAssalariado.setNome("Veronica");
+        empregadoAssalariado.setSobrenome("Soares");
+        empregadoAssalariado.setCpf("2");
+        System.out.println(empregadoAssalariado.getNome());
+        System.out.println(empregadoAssalariado.getSobrenome());
+        System.out.println(empregadoAssalariado.getCpf());
 
         System.out.println("\n");
         
         //Comissionado
         System.out.println(empregadoComissionado.getClass().getSimpleName());
-
-
+        empregadoComissionado.setNome("Sophia");
+        empregadoComissionado.setSobrenome("Silva");
+        empregadoComissionado.setCpf("3");
+        System.out.println(empregadoComissionado.getNome());
+        System.out.println(empregadoComissionado.getSobrenome());
+        System.out.println(empregadoComissionado.getCpf());
 
         System.out.println("\n");
     }
