b28042d034e9eba6813727cc2634e1084ca79183
Leonardo Soares <140261155+LeonardoHSSoares@users.noreply.github.com>
2024-08-17 13:21:00 -0300

    fix

--- EBAC/mod23clone/tentativa1/demo/src/main/java/com/example1/TesteSaudacao.java
+++
@@ -1,9 +0,0 @@
-package com.example1;
-
-public class TesteSaudacao {
-
-    @Test
-    public void testar() {
-        Main testeSaudacao = new Main();
-    }
-}
--- EBAC/mod23clone/tentativa2/testesClasses/src/TestAll.java
+++
@@ -1,9 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-@RunWith(Suite.class)
-@SuiteClasses({TesteSaudacao.class,TesteSaudacao2.class})
-public class TestAll {
-
-}
