99e5bac9c622b0a5bf5ad0e68a14a4045cf0dc70
LeonardoHSSoares <mascoty2005@gmail.com>
2025-03-06 18:58:25 -0300

    🎉 init(Modulo de Testes):
    exercicios

---
+++ EBAC/estrutura_de_dados/mod5-testes/tests/src/main/java/com/modulo_tests/tests/TestsApplication.java
@@ -0,0 +1,13 @@
+package com.modulo_tests.tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsApplication.class, args);
+	}
+
+}
---
+++ EBAC/estrutura_de_dados/mod5-testes/tests/src/test/java/com/modulo_tests/tests/TestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modulo_tests.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
