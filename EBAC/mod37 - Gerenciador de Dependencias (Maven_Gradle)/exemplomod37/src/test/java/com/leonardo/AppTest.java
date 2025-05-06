75d05d43683f6faf9f61ccae094920e3a1677b3e
LeonardoHSSoares <mascoty2005@gmail.com>
2025-05-06 12:41:40 -0300

    🎉 init(Moludo 37): G. de Dependencia
    Maven/Gradle

---
+++ EBAC/mod37 - Gerenciador de Dependencias (Maven_Gradle)/exemplomod37/src/test/java/com/leonardo/AppTest.java
@@ -0,0 +1,19 @@
+package com.leonardo;
+
+import static org.junit.gen5.api.Assertions.assertTrue;
+import org.junit.gen5.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
