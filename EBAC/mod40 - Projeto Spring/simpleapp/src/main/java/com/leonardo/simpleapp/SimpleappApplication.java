5fdc8fc7fb99f6495eda208453b6bf3be29a9058
LeonardoHSSoares <mascoty2005@gmail.com>
2025-05-28 18:01:13 -0300

    🦄 refactor(Tarefa Mod40):
    projeto simples springboot

---
+++ EBAC/mod40 - Projeto Spring/simpleapp/src/main/java/com/leonardo/simpleapp/SimpleappApplication.java
@@ -0,0 +1,15 @@
+package com.leonardo.simpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleappApplication.class, args);
+		System.out.println("SimpleappApplication started successfully!");
+		
+	}
+
+}
---
+++ EBAC/mod40 - Projeto Spring/simpleapp/src/test/java/com/leonardo/simpleapp/SimpleappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.simpleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
