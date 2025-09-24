4699ad48c8313897e1fafeec343cbf8e3d1fa1fa
LeonardoHSSoares <mascoty2005@gmail.com>
2025-09-24 18:38:15 -0300

    Refactor web application structure and configuration
    - Updated web.xml to use XML version 1.0 and changed to Java EE 2.5 schema.
    - Removed index.jsp file as it was no longer needed.
    - Uncommented and implemented test methods in ClienteTest.java for client management functionalities.
    - Added new configuration files: beans.xml, faces-config.xml, jboss-web.xml for JSF and CDI support.
    - Created list.xhtml for displaying and managing clients with PrimeFaces components.
    - Added PostgreSQL module configuration in WildFly for database connectivity.
    - Updated standalone.xml with datasource configurations and security realms for application management.

--- EBAC/mod38 - Criando um projeto monolítico para web/beta/demo/src/test/java/com/leonardo/ClienteTest.java
+++ EBAC/mod38 - Criando um projeto monolítico para web/beta/demo/src/test/java/com/leonardo/ClienteTest.java
@@ -10,48 +10,48 @@ import com.leonardo.exceptions.TipoChaveNaoEncontradaException;
 
 public class ClienteTest {
 
-    // @Test
-    // public void cadastrarCliente() throws TipoChaveNaoEncontradaException, DAOException {
-    //     Cliente cliente = new Cliente();
-    //     cliente.setNome("Leonardo");
-    //     cliente.setCpf(12345678901L);
-    //     cliente.setTel(11999999999L);
-    //     cliente.setEmail("asdsd@gmail.com");
-    //     cliente.setEnd("Rua A");
-    //     cliente.setNumero(123);
-    //     cliente.setCidade("São Paulo");
-    //     cliente.setEstado("SP");
+    @Test
+    public void cadastrarCliente() throws TipoChaveNaoEncontradaException, DAOException {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Leonardo");
+        cliente.setCpf(12345678901L);
+        cliente.setTel(11999999999L);
+        cliente.setEmail("asdsd@gmail.com");
+        cliente.setEnd("Rua A");
+        cliente.setNumero(123);
+        cliente.setCidade("São Paulo");
+        cliente.setEstado("SP");
 
-    //     ClienteDAO dao = new ClienteDAO();
-    //     Cliente salvo = dao.cadastrar(cliente);
+        ClienteDAO dao = new ClienteDAO();
+        Cliente salvo = dao.cadastrar(cliente);
 
-    //     Assert.assertNotNull(salvo);
-    //     Assert.assertEquals("Leonardo", salvo.getNome());
-    // }
+        Assert.assertNotNull(salvo);
+        Assert.assertEquals("Leonardo", salvo.getNome());
+    }
 
-    // @Test
-    // public void consultarCliente() throws Exception {
-    //     ClienteDAO dao = new ClienteDAO();
-    //     Cliente cliente = dao.consultar(12345678901L);
-    //     Assert.assertNotNull(cliente);
-    //     Assert.assertEquals("Leonardo", cliente.getNome());
-    // }
+    @Test
+    public void consultarCliente() throws Exception {
+        ClienteDAO dao = new ClienteDAO();
+        Cliente cliente = dao.consultar(12345678901L);
+        Assert.assertNotNull(cliente);
+        Assert.assertEquals("Leonardo", cliente.getNome());
+    }
 
-    // @Test
-    // public void alterarCliente() throws Exception {
-    //     ClienteDAO dao = new ClienteDAO();
-    //     Cliente cliente = dao.consultar(12345678901L);
-    //     cliente.setNome("Leonardo Silva");
-    //     Cliente alterado = dao.alterar(cliente);
-    //     Assert.assertEquals("Leonardo Silva", alterado.getNome());
-    // }
+    @Test
+    public void alterarCliente() throws Exception {
+        ClienteDAO dao = new ClienteDAO();
+        Cliente cliente = dao.consultar(12345678901L);
+        cliente.setNome("Leonardo Silva");
+        Cliente alterado = dao.alterar(cliente);
+        Assert.assertEquals("Leonardo Silva", alterado.getNome());
+    }
 
-    // @Test
-    // public void excluirCliente() throws Exception {
-    //     ClienteDAO dao = new ClienteDAO();
-    //     Cliente cliente = dao.consultar(12345678901L);
-    //     dao.excluir(cliente);
-    //     Cliente excluido = dao.consultar(12345678901L);
-    //     Assert.assertNull(excluido);
-    // }
+    @Test
+    public void excluirCliente() throws Exception {
+        ClienteDAO dao = new ClienteDAO();
+        Cliente cliente = dao.consultar(12345678901L);
+        dao.excluir(cliente);
+        Cliente excluido = dao.consultar(12345678901L);
+        Assert.assertNull(excluido);
+    }
 }
